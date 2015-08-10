1d88900bc54236978d7df49c592cb87514e591fc
Anton Straka <anton_straka@datalan.sk>
2015-08-10 19:48:24 +0200

    Throws formatting

--- src/main/java/sk/antons/util/logging/SimpleLineFormatter.java
+++ src/main/java/sk/antons/util/logging/SimpleLineFormatter.java
@@ -16,6 +16,8 @@
 
 package sk.antons.util.logging;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
 import java.util.logging.Formatter;
 import java.util.logging.LogManager;
@@ -102,6 +104,18 @@ public class SimpleLineFormatter extends Formatter {
         for(AbstractAppender appender : appenders) {
             appender.append(sb, record);
         }
+
+        if(record.getThrown() != null) {
+    		sb.append('\n').append(record.getThrown()).append('\n');
+            try {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                record.getThrown().printStackTrace(pw);
+                pw.close();
+                sb.append(sw.toString());
+            } catch(Exception ex) { }
+        }
+
 		sb.append("\n");
 
         return sb.toString();
